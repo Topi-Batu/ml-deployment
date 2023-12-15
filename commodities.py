4491f4caff88c0698cf117fbb0ea38b0f44a9873
rizkiwijanarko <rizkiwijanarko@students.unnes.ac.id>
2023-12-15 20:42:28 +0700

    tes

--- commodities.py
+++ commodities.py
@@ -5,3 +5,6 @@ def cabai_rawit_merah(num_prediction):
 
 # def cabai_rawit_merah(num_prediction):
 #     return util(dataset_name="Cabai_Merah_Keriting", model_name="cabai-rawit-merah", num_prediction=num_prediction)
+
+def cabai_merah_keriting(num_prediction):
+    return util(dataset_name="Cabai_Merah_Keriting", model_name="Cabai_Merah_Keriting-LSTM_2.0", num_prediction=num_prediction)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from commodities import cabai_rawit_merah
+from commodities import cabai_rawit_merah, cabai_merah_keriting
 
 app = FastAPI()
 
@@ -10,3 +10,7 @@ def read_root():
 @app.get("/cabai-rawit-merah/{num_prediction}")
 def cabai_rawit_merah_prediction(num_prediction: int):
     return cabai_rawit_merah(num_prediction=num_prediction)
+
+@app.get("/cabai-merah-keriting/{num_prediction}")
+def cabai_merah_keriting_prediction(num_prediction: int):
+    return cabai_merah_keriting(num_prediction=num_prediction)
