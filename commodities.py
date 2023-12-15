ab3e32c5f2a62e60f34e80aedd30d758fca43a7b
rizkiwijanarko <rizkiwijanarko@students.unnes.ac.id>
2023-12-15 20:41:38 +0700

    Menambahkan dataset, model, dan fix bug

--- commodities.py
+++ commodities.py
@@ -1,7 +1,7 @@
 from utils import util
 
 def cabai_rawit_merah(num_prediction):
-    return util(dataset_name="Cabai_Merah_Keriting", model_name="cabai-rawit-merah", num_prediction=num_prediction)
+    return util(dataset_name="Cabai_Rawit_Merah", model_name="Cabai Rawit Merah-GRU_1.0", num_prediction=num_prediction)
 
 # def cabai_rawit_merah(num_prediction):
 #     return util(dataset_name="Cabai_Merah_Keriting", model_name="cabai-rawit-merah", num_prediction=num_prediction)
